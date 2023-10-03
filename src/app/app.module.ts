6ded9af1154155be8d407da1ecbf229f33b64b71
Lucas Cipriano <lucas.cipra@gmail.com>
2023-10-03 15:21:43 -0300

    Projeto PSN store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardsLabelComponent } from './components/cards/cards-label/cards-label.component';
+import { CardsPricingComponent } from './components/cards/cards-pricing/cards-pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardsComponent,
+    MenuBarComponent,
+    CardsLabelComponent,
+    CardsPricingComponent
   ],
   imports: [
     BrowserModule
